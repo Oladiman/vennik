e4b766dc923f92e031e6ef965cffae971b1b4e8f
Oladiman <astonishingdami9@gmail.com>
2018-09-17 10:01:53 +0100

    go

--- vennik/admin.py
+++ vennik/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from vennik.models import UserProfile,Location
 
 # Register your models here.
 admin.site.site_header='Venik Administration'
 admin.site.index_title = 'Venik Administration'
+admin.site.register(UserProfile)
+admin.site.register(Location)
--- vennik/models.py
+++ vennik/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    description = models.CharField(max_length=120 ,default='' )
+    email = models.CharField(max_length=120 ,default='')
+    phone =models.IntegerField(default=0)
+    department = models.CharField(max_length=100,default='',)
+
+class Location(models.Model):
+    LocationName=models.CharField(max_length=50, default='')
+    # pricerange=models.IntegerField(default='0')
